aec551ad7ca8968c02789248aeb08de676566231
Cyberkrane <cyberkrane@gmail.com>
2022-07-01 15:43:04 -0300

    agregué las paginas de login, register y 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,33 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 
 import { LoginComponent } from './auth/login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './pages/pages.component';
 
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
+  {path: '', 
+  component: PagesComponent,
+  children:[
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'grafica1', component: Grafica1Component},
+     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+},
+  
+  
   {path: 'login', component: LoginComponent},
-  {path: 'progress', component: ProgressComponent},
-  {path: 'grafica1', component: Grafica1Component},
-  {path: '', redirectTo: '/dasboard', pathMatch: 'full'},
+  {path: 'register', component: RegisterComponent},
+
+ 
   {path: '**', component: NotFoundComponent}
 ];
 
