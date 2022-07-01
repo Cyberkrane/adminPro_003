fa59a94ba815639a1275c09078d9de65a9c650b0
Cyberkrane <cyberkrane@gmail.com>
2022-07-01 14:36:28 -0300

    carga inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'progress', component: ProgressComponent},
+  {path: 'grafica1', component: Grafica1Component},
+  {path: '', redirectTo: '/dasboard', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
