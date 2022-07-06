cdf3ec84187d6e991a237ea4e4f7d599ac1ccd4b
Cyberkrane <cyberkrane@gmail.com>
2022-07-06 19:00:51 -0300

    up002

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,29 @@
+import { PagesRoutingModule } from './pages/pages-routing.module';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
 import { LoginComponent } from './auth/login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { PagesComponent } from './pages/pages.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-  {path: '', 
-  component: PagesComponent,
-  children:[
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Grafica1Component},
-     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  ]
-},
-  
-  
+ 
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-
- 
   {path: '**', component: NotFoundComponent}
 ];
 
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
